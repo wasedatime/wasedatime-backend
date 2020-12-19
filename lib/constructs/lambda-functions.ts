2795fa44f6746412430724623b1c94f008021ba6
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2020-12-20 02:27:17 +0900

    fix: typo in lambda-functions.ts

--- lib/constructs/lambda-functions.ts
+++ lib/constructs/lambda-functions.ts
@@ -64,7 +64,7 @@ export class SyllabusScraper extends cdk.Construct {
     }
 }
 
-export class slackWebhookPublisher extends cdk.Construct {
+export class SlackWebhookPublisher extends cdk.Construct {
 
     readonly baseFunction: Function;
 
