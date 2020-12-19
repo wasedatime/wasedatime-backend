8ec981aa1ab6d22ed742930f9066908cda233ce2
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2020-12-20 02:20:13 +0900

    feat: add cognito resources fix: admin.ts

--- lib/stacks/admin.ts
+++ lib/stacks/admin.ts
@@ -4,30 +4,29 @@ import {AdminLayer} from "../architecture/layers";
 import {OperationInterface} from "../architecture/interfaces";
 import {OperationEndpoint} from "../configs/registry";
 import {
-    AbstractTaskManager,
+    AbstractStatusNotifier,
     AmplifyBuildStatusNotifier,
-    SyllabusScraperStatusNotifier,
-    SyllabusScraperTaskManger,
-    TaskManager
-} from "../constructs/task-managers";
+    StackStatusNotifier,
+    StatusNotifier,
+    SyllabusScraperStatusNotifier
+} from "../constructs/status-notifier";
 
 export class WasedaTimeAdminLayer extends AdminLayer {
 
-    readonly taskManagers: { [name in TaskManager]?: AbstractTaskManager } = {};
+    readonly taskManagers: { [name in StatusNotifier]?: AbstractStatusNotifier } = {};
 
     constructor(scope: cdk.Construct, id: string, operationInterface: OperationInterface, props: cdk.StackProps) {
+
         super(scope, id, operationInterface, props);
 
-        this.taskManagers[TaskManager.SCRAPER_TASK] = new SyllabusScraperTaskManger(this, 'scraper-task', {
-            target: this.operationInterface.getEndpoint(OperationEndpoint.SYLLABUS)
-        });
-
-        this.taskManagers[TaskManager.BUILD_STATUS] = new AmplifyBuildStatusNotifier(this, 'build-notifier', {
+        this.taskManagers[StatusNotifier.BUILD_STATUS] = new AmplifyBuildStatusNotifier(this, 'build-notifier', {
             target: this.operationInterface.getEndpoint(OperationEndpoint.APP)
         });
 
-        this.taskManagers[TaskManager.SCRAPER_STATUS] = new SyllabusScraperStatusNotifier(this, 'scraper-notifier', {
+        this.taskManagers[StatusNotifier.SCRAPER_STATUS] = new SyllabusScraperStatusNotifier(this, 'scraper-notifier', {
             target: this.operationInterface.getEndpoint(OperationEndpoint.SYLLABUS)
         });
+
+        this.taskManagers[StatusNotifier.SCRAPER_STATUS] = new StackStatusNotifier(this, 'scraper-notifier', {});
     }
 }
