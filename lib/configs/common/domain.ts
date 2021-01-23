6d4ae32758fde21e056578b47c9f15e9ac91cb1f
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2021-01-24 02:49:37 +0900

    feat: add domain.ts
    Signed-off-by: AustinZhu <42071208+AustinZhu@users.noreply.github.com>

---
+++ lib/configs/common/domain.ts
@@ -0,0 +1,9 @@
+export const ROOT_DOMAIN: string = "wasedatime.com";
+
+export const API_DOMAIN: string = "api.wasedatime.com";
+
+export const AUTH_DOMAIN: string = "auth.wasedatime.com";
+
+export const ES_DOMAIN: string = "elastic.wasedatime.com";
+
+export const OPENAPI_DOMAIN: string = "openapi.wasedatime.com";
