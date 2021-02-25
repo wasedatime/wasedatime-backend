72f7a19c399106f3faa9c8784a17ad6f8d86222a
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2021-02-26 02:13:16 +0900

    fix: connection type
    Signed-off-by: AustinZhu <42071208+AustinZhu@users.noreply.github.com>

--- lib/constructs/business/graphql-api-service.ts
+++ lib/constructs/business/graphql-api-service.ts
@@ -17,6 +17,7 @@ import {
     list_int,
     list_of,
     list_string,
+    PageInfo,
     required,
     required_string,
     string,
@@ -54,7 +55,7 @@ export class SyllabusApiService extends cdk.Construct {
             name: "SyllabusTable",
         });
 
-        const schoolType = new EnumType('School', {
+        const School = new EnumType('School', {
             definition: [
                 "PSE", "FSE", "SSS", "SILS", "CSE", "ASE", "LAW", "CMS", "HSS", "EDU", "SOC", "HUM", "SPS", "CJL",
                 "GEC", "CIE", "ART", "G_SPS", "G_SE", "G_LAW", "G_LAS", "G_SC", "G_EDU", "G_HUM", "G_SSS", "G_SAPS",
@@ -62,33 +63,33 @@ export class SyllabusApiService extends cdk.Construct {
                 "G_SICCS",
             ],
         });
-        const evalType = new ObjectType("Evaluation", {
+        const Eval = new ObjectType("Evaluation", {
             definition: {
                 type: int,
                 percent: int,
                 criteria: string,
             },
         });
-        const occurrenceType = new ObjectType("Occurrence", {
+        const Occurrence = new ObjectType("Occurrence", {
             definition: {
                 day: int,
                 period: int,
                 location: string,
             },
         });
-        const courseType = new ObjectType("Course", {
+        const Course = new ObjectType("Course", {
             definition: {
                 id: string,
                 category: string,
                 code: string,
                 credit: int,
-                evals: list_of(evalType),
+                evals: list_of(Eval),
                 instructor: string,
                 lang: list_int,
                 level: int,
                 minYear: int,
-                occurrences: list_of(occurrenceType),
-                school: schoolType.attribute(),
+                occurrences: list_of(Occurrence),
+                school: School.attribute(),
                 subtitle: string,
                 term: string,
                 title: string,
@@ -97,13 +98,13 @@ export class SyllabusApiService extends cdk.Construct {
             },
         });
 
-        const evalFilter = new InputType("EvalFilter", {
+        const EvalFilter = new InputType("EvalFilter", {
             definition: {
                 type: int,
                 percent: int,
             },
         });
-        const filterForm = new InputType("FilterForm", {
+        const FilterForm = new InputType("FilterForm", {
             definition: {
                 semester: list_string,
                 lang: list_int,
@@ -111,21 +112,21 @@ export class SyllabusApiService extends cdk.Construct {
                 period: list_int,
                 min_year: list_int,
                 credit: list_int,
-                eval: list_of(evalFilter),
+                eval: list_of(EvalFilter),
                 type: list_int,
                 level: list_int,
             },
         });
 
-        const courseConnection = generateConnectionAndEdge({base: courseType, target: courseType}).connection;
-        const courseEdge = generateConnectionAndEdge({base: courseType, target: courseType}).edge;
+        const CourseConnection = generateConnectionAndEdge({base: Course, target: Course}).connection;
+        const CourseEdge = generateConnectionAndEdge({base: Course, target: Course}).edge;
 
-        [schoolType, evalType, occurrenceType, courseType, evalFilter, filterForm, courseConnection, courseEdge].forEach(
+        [School, Eval, Occurrence, Course, Eval, FilterForm, CourseConnection, CourseEdge, PageInfo].forEach(
             (type) => props.apiEndpoint.addType(type),
         );
 
         props.apiEndpoint.addQuery('getCourses', new ResolvableField({
-            returnType: list_of(courseType),
+            returnType: list_of(Course),
             dataSource: dataSource,
             args: {
                 id: required_string,
@@ -134,10 +135,10 @@ export class SyllabusApiService extends cdk.Construct {
             responseMappingTemplate: MappingTemplate.dynamoDbResultItem(),
         }));
         props.apiEndpoint.addQuery('filterCourses', new ResolvableField({
-            returnType: courseConnection.attribute(),
+            returnType: CourseConnection.attribute(),
             dataSource: dataSource,
             args: {
-                form: required(filterForm),
+                form: required(FilterForm),
                 after: string,
                 first: int,
                 before: string,
