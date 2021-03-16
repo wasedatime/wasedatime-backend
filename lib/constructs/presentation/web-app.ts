051a100e2f5d60891f9e4c3bbb1e065fdab74cb6
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2021-03-17 00:33:55 +0900

    hotfix: resolve cyclic dependencies
    Signed-off-by: AustinZhu <42071208+AustinZhu@users.noreply.github.com>

--- lib/constructs/presentation/web-app.ts
+++ lib/constructs/presentation/web-app.ts
@@ -1,5 +1,5 @@
 import * as cdk from "@aws-cdk/core";
-import {App, Branch, Domain} from "@aws-cdk/aws-amplify";
+import {App, Branch, CustomRule, Domain, RedirectStatus} from "@aws-cdk/aws-amplify";
 
 import {developerAuth, webappSiteRules} from "../../configs/amplify/website";
 import {
@@ -79,7 +79,6 @@ export class AmplifyWebApp extends AbstractWebApp {
         this.domain = this.app.addDomain('domain', {
             domainName: ROOT_DOMAIN,
             subDomains: [
-                // {branch: devBranch, prefix: "dev"},
                 {branch: masterBranch, prefix: ''},
                 {branch: masterBranch, prefix: 'www'},
             ],
@@ -168,25 +167,25 @@ export class AmplifyMonoWebApp extends AbstractWebApp {
             },
         });
 
-        // microApp.addBranch('master', {
-        //     autoBuild: true,
-        //     branchName: "master",
-        //     stage: "PRODUCTION",
-        //     buildSpec: microAppBuildSpec(name),
-        // }).addEnvironment("REACT_APP_API_BASE_URL", `https://${this.appProps.apiDomain}/v1`);
-        // this.app.addCustomRule(new CustomRule({
-        //     source: `/${name}/<*>`,
-        //     target: `https://master.${microApp.defaultDomain}/<*>`,
-        //     status: RedirectStatus.REWRITE,
-        // }));
-        //
-        // microApp.addBranch('dev', {
-        //     autoBuild: true,
-        //     branchName: "develop",
-        //     stage: "DEVELOPMENT",
-        //     buildSpec: microAppDevBuildSpec(name),
-        // }).addEnvironment("REACT_APP_API_BASE_URL", `https://${this.appProps.apiDomain}/staging`);
-        // this.app.addEnvironment(`MF_${name.toUpperCase()}_DOMAIN`, microApp.defaultDomain);
+        microApp.addBranch('master', {
+            autoBuild: true,
+            branchName: "master",
+            stage: "PRODUCTION",
+            buildSpec: microAppBuildSpec(name),
+        }).addEnvironment("REACT_APP_API_BASE_URL", `https://${this.appProps.apiDomain}/v1`);
+        this.app.addCustomRule(new CustomRule({
+            source: `/${name}/<*>`,
+            target: `https://master.${microApp.defaultDomain}/<*>`,
+            status: RedirectStatus.REWRITE,
+        }));
+
+        microApp.addBranch('dev', {
+            autoBuild: true,
+            branchName: "develop",
+            stage: "DEVELOPMENT",
+            buildSpec: microAppDevBuildSpec(name),
+        }).addEnvironment("REACT_APP_API_BASE_URL", `https://${this.appProps.apiDomain}/staging`);
+        this.app.addEnvironment(`MF_${name.toUpperCase()}_DOMAIN`, microApp.defaultDomain);
 
         return this;
     }
