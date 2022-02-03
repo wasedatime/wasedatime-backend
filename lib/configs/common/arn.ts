a661f2bace720641c15c57ec83f1b28745851f62
Austin Zhu <42071208+AustinZhu@users.noreply.github.com>
2022-02-03 23:11:08 +0900

    fix: renew auth cert (#127)

--- lib/configs/common/arn.ts
+++ lib/configs/common/arn.ts
@@ -1,3 +1,3 @@
-export const AUTH_CERT_ARN = 'arn:aws:acm:us-east-1:564383102056:certificate/7e29831d-9eb9-4212-9856-4f5fd0d3cafe';
+export const AUTH_CERT_ARN = 'arn:aws:acm:us-east-1:564383102056:certificate/a79577a1-c32e-4ed9-9201-82325ab657e4';
 
 export const CF_TOPIC_ARN = 'arn:aws:sns:ap-northeast-1:564383102056:cfn-alert';
