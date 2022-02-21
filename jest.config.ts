1ea9c00fea632b100b4b8038bb84750c51b212e9
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2022-02-22 02:58:51 +0900

    fix: update jest config
    Signed-off-by: AustinZhu <42071208+AustinZhu@users.noreply.github.com>

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,12 @@
 import type { Config } from '@jest/types';
 
-export const config: Config.InitialOptions = {
+const config: Config.InitialOptions = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  globals: {
-    'ts-jest': {
-      tsconfig: {
-        rootDir: '.',
-      },
-    },
-  },
 };
+
+export default config;
