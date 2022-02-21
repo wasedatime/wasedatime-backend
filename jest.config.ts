13ead2b14fc33651c6a6a307cf388092e3ba8e0e
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2022-02-22 02:03:30 +0900

    fix(lint): reformat code
    Signed-off-by: AustinZhu <42071208+AustinZhu@users.noreply.github.com>

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,9 @@
 import type { Config } from '@jest/types';
 
-const config: Config.InitialOptions = {
-    roots: ['<rootDir>/test'],
-    testMatch: ['**/*.test.ts'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
+export const config: Config.InitialOptions = {
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 };
-
-export default config;
