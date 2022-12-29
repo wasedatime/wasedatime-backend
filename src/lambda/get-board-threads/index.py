be39d0359e2c94172a558df6244e4f90784e471e
Jason Park <joonbin0319@gmail.com>
2022-12-29 22:33:01 +0900

    fix: fix git branch merging error

---
+++ src/lambda/get-board-threads/index.py
@@ -0,0 +1,26 @@
+from boto3.dynamodb.conditions import Key
+import boto3
+from datetime import datetime
+from utils import JsonPayloadBuilder, table, resp_handler
+
+
+@resp_handler
+def get_board_threads(board_id):
+
+    results = table.query(KeyConditionExpression=Key(
+        "board_id").eq(board_id))["Items"]
+    if not results:
+        raise LookupError
+
+    body = JsonPayloadBuilder().add_status(
+        True).add_data(results).add_message('').compile()
+    return body
+
+
+def handler(event, context):
+
+    params = {
+        "board_id": event["pathParameters"]["board_id"]
+    }
+
+    return get_board_threads(**params)
---
+++ src/lambda/post-thread/utils.py
@@ -0,0 +1,123 @@
+import base64
+import boto3
+import json
+import logging
+import os
+from decimal import Decimal
+from datetime import datetime
+import uuid
+from google.cloud import translate
+from google.oauth2 import service_account
+
+# AWS DynamoDB Resources
+db = boto3.resource("dynamodb", region_name="ap-northeast-1")
+table = db.Table(os.getenv('TABLE_NAME'))
+
+# Google Translation client and configs
+acct_info = json.loads(base64.b64decode(
+    os.environ.get('GOOGLE_API_SERVICE_ACCOUNT_INFO')))
+credentials = service_account.Credentials.from_service_account_info(acct_info)
+client = translate.TranslationServiceClient(credentials=credentials)
+parent = "projects/wasedatime/locations/global"
+
+# Supported languages
+langs = ['en', 'ja', 'ko', 'zh-CN', 'zh-TW']
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+class JsonPayloadBuilder:
+    payload = {}
+
+    def add_status(self, success):
+        self.payload['success'] = success
+        return self
+
+    def add_data(self, data):
+        self.payload['data'] = data
+        return self
+
+    def add_message(self, msg):
+        self.payload['message'] = msg
+        return self
+
+    def compile(self):
+        return json.dumps(self.payload, cls=DecimalEncoder, ensure_ascii=False).encode('utf8')
+
+
+def api_response(code, body):
+    return {
+        "isBase64Encoded": False,
+        "statusCode": code,
+        'headers': {
+            "Access-Control-Allow-Origin": '*',
+            "Content-Type": "application/json",
+            "Referrer-Policy": "origin"
+        },
+        "multiValueHeaders": {"Access-Control-Allow-Methods": ["POST", "OPTIONS", "GET", "PATCH", "DELETE"]},
+        "body": body
+    }
+
+
+def resp_handler(func=None, headers=None):
+    def handle(*args, **kwargs):
+        try:
+            resp = func(*args, **kwargs)
+            return api_response(200, resp)
+        except LookupError:
+            resp = JsonPayloadBuilder().add_status(False).add_data(None) \
+                .add_message("Not found").compile()
+            return api_response(404, resp)
+        except Exception as e:
+            logging.error(str(e))
+            resp = JsonPayloadBuilder().add_status(False).add_data(None) \
+                .add_message("Internal error, please contact bugs@wasedatime.com.").compile()
+            return api_response(500, resp)
+
+    return handle
+
+
+def translate_text(text):
+    src_lang = client.detect_language(request={
+        "parent": parent,
+        "content": text,
+        "mime_type": "text/plain"
+    }).languages[0].language_code
+
+    translations = {}
+
+    for lang in langs:
+        if lang == src_lang:
+            translations[lang] = text
+            continue
+
+        translated = client.translate_text(request={
+            "parent": parent,
+            "contents": [text],
+            "mime_type": "text/plain",
+            "source_language_code": src_lang,
+            "target_language_code": lang
+        }).translations[0].translated_text
+
+        translations[lang] = translated or ''
+
+    return src_lang, translations
+
+
+def build_thread_id(uid):
+
+    unique_id = str(uuid.uuid4())
+
+    ts = datetime.now().strftime('%Y%m%d%H%M%S')
+
+    first_half = uid[:5]
+    second_half = unique_id[5:]
+
+    thread_id = f"{ts}{first_half}{second_half}"
+
+    return thread_id
