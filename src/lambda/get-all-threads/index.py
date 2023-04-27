11e11457dd8f4a27f36d44f2afd78c0abd15754f
Jason Park <joonbin0319@gmail.com>
2023-04-27 23:13:53 +0900

    fix: fixing api schema

--- src/lambda/get-all-threads/index.py
+++ src/lambda/get-all-threads/index.py
@@ -6,7 +6,6 @@ from utils import JsonPayloadBuilder, table, resp_handler
 
 @resp_handler
 def get_all_threads():
-    # respons will be table scan by TableName and return first 50 items
 
     response = table.scan(TableName=table, Limit=50)
     items = response['Items']
