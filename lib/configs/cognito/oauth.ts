3a522715899818596a05600198ef5d3f310cf514
Jason Park <joonbin0319@gmail.com>
2023-08-06 23:56:16 +0900

    feat: adding oauth for flutter app

--- lib/configs/cognito/oauth.ts
+++ lib/configs/cognito/oauth.ts
@@ -1,7 +1,21 @@
 export const GOOGLE_OAUTH_CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID!;
 
-export const GOOGLE_OAUTH_CLIENT_SECRET = process.env.GOOGLE_OAUTH_CLIENT_SECRET!;
+export const GOOGLE_OAUTH_CLIENT_SECRET =
+  process.env.GOOGLE_OAUTH_CLIENT_SECRET!;
 
-export const CALLBACK_URLS = ['https://wasedatime.com/verify', 'https://dev.wasedatime.com/verify', 'localhost:3000/verify'];
+export const CALLBACK_URLS = [
+  'https://wasedatime.com/verify',
+  'https://dev.wasedatime.com/verify',
+  'localhost:3000/verify',
+  'wasedati',
+];
 
-export const LOGOUT_URLS = ['https://wasedatime.com/', 'https://dev.wasedatime.com/', 'localhost:3000/'];
+export const LOGOUT_URLS = [
+  'https://wasedatime.com/',
+  'https://dev.wasedatime.com/',
+  'localhost:3000/',
+];
+
+export const FLUTTER_CALLBACK_URL = ['wasedatime://verify'];
+
+export const FLUTTER_LOGOUT_URL = ['wasedatime://'];
