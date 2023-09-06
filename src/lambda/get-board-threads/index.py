8595e2dfdaa5c35162926696acc0a6966db98266
Jason Park <joonbin0319@gmail.com>
2023-09-06 21:29:53 +0900

    feat: adding mod flag to board threads

--- src/lambda/get-board-threads/index.py
+++ src/lambda/get-board-threads/index.py
@@ -5,12 +5,17 @@ from utils import JsonPayloadBuilder, table, resp_handler
 
 
 @resp_handler
-def get_board_threads(board_id):
+def get_board_threads(board_id, uid=''):
 
     results = table.query(KeyConditionExpression=Key(
         "board_id").eq(board_id), ScanIndexForward=False)["Items"]
     if not results:
         raise LookupError
+
+    for item in results:
+        item['mod'] = False
+        if 'uid' in item and item['uid'] == uid:
+            item['mod'] = True
 
     body = JsonPayloadBuilder().add_status(
         True).add_data(results).add_message('').compile()
@@ -22,5 +27,7 @@ def handler(event, context):
     params = {
         "board_id": event["pathParameters"]["board_id"]
     }
+    if "uid" in event["queryStringParameters"]:
+        params["uid"] = event["queryStringParameters"]["uid"]
 
     return get_board_threads(**params)
