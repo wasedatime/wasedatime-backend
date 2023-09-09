09fa96919163e2f0a913d5335c5f53cba0c4befd
Jason Park <joonbin0319@gmail.com>
2023-09-09 17:31:37 +0900

    feat: fixing get functionality

--- src/lambda/get-all-threads/index.py
+++ src/lambda/get-all-threads/index.py
@@ -5,56 +5,35 @@ from utils import JsonPayloadBuilder, table, resp_handler
 
 
 @resp_handler
-def get_all_threads(uid="", board_id=None, last_evaluated_key=None, limit=10):
-
-    if board_id:
-        query_params = {
-            'KeyConditionExpression': Key('board_id').eq(board_id),
-            'Limit': limit
-        }
-
-        # If there's a last evaluated key, add it to the query parameters
-        if last_evaluated_key:
-            query_params['ExclusiveStartKey'] = {
-                'board_id': board_id,
-                'thread_id': last_evaluated_key
-            }
-
-        response = table.query(**query_params)
-    else:
-        # If board_id is not provided, scan may be inefficient for large tables
-        response = table.scan()
-
+def get_all_threads(uid="", index=0, num=5):
+    response = table.scan()
     items = response['Items']
 
-    # Add mod field
-    for item in items:
+    start_index = index
+
+    end_index = start_index + num
+    paginated_items = items[start_index:end_index]
+
+    for item in paginated_items:
         item['mod'] = False
         if 'uid' in item and item['uid'] == uid:
             item['mod'] = True
 
-    result = {
-        'items': items,
-        'last_evaluated_key': response.get('LastEvaluatedKey', {}).get('thread_id')
-    }
-
     body = JsonPayloadBuilder().add_status(
-        True).add_data(result).add_message('').compile()
+        True).add_data(paginated_items).add_message('').compile()
 
     return body
 
 
 def handler(event, context):
     uid = ""
-    board_id = None
-    last_evaluated_key = None
-    limit = 10
+    index = 1
+    num = 5
 
     if "queryStringParameters" in event:
         params = event["queryStringParameters"]
         uid = params.get("uid", "")
-        board_id = params.get("board_id", None)
-        last_evaluated_key = params.get("last_evaluated_key", None)
-        limit = int(params.get("limit", 10))
+        index = int(params.get("page", 1))
+        num = int(params.get("num", 5))
 
-    return get_all_threads(uid, board_id, last_evaluated_key, limit)
+    return get_all_threads(uid, index, num)
