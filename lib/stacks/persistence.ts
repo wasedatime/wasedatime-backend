09941a29dd89c86f2cf7df315f3c1141cd2fb6f0
Jason Park <joonbin0319@gmail.com>
2023-09-12 21:07:33 +0900

    feat: rebuilding table what just happened

--- lib/stacks/persistence.ts
+++ lib/stacks/persistence.ts
@@ -60,7 +60,6 @@ export class WasedaTimePersistenceLayer extends PersistenceLayer {
       DataEndpoint.SYLLABUS,
       syllabusDataPipeline.dataWarehouse.bucketName,
     );
-
     this.dataInterface.setEndpoint(
       DataEndpoint.THREAD,
       dynamoDatabase.tables[Collection.THREAD].tableName,
