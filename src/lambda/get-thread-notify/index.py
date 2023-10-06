aa3965c66fd0945be786b8f0f74b86d168151a83
JasonNotJson <joonbin0319@gmail.com>
2023-10-06 20:46:04 +0900

    feat: adding lambda for notification

---
+++ src/lambda/get-thread-notify/index.py
@@ -0,0 +1,51 @@
+from boto3.dynamodb.conditions import Key, Attr
+import boto3
+from datetime import datetime
+from utils import JsonPayloadBuilder, table, resp_handler
+
+
+@resp_handler
+def get_thread_notify(Univ_Id=1):
+
+    # Query the GSI
+    response = table.query(
+        IndexName='UnivIDbyThreadIDIndex',
+        KeyConditionExpression=Key('Univ_Id').eq(Univ_Id),
+        FilterExpression=Attr('new_comment').eq(True),
+        ScanIndexForward=False  # Sorting by thread_id in descending order
+    )
+
+    results = response['Items']
+
+    # Count the threads with new comments
+    new_comment_count = len(results)
+
+    # Collect the thread_ids
+    thread_ids = [item['thread_id'] for item in results]
+
+    # Determine the response data based on new_comment_count
+    response_data = {
+        'thread_ids': thread_ids,
+        'new_comment_count': new_comment_count,
+        'new_comment_flag': new_comment_count >= 1
+    }
+
+    body = JsonPayloadBuilder().add_status(True)\
+                               .add_data(response_data)\
+                               .add_message('').compile()
+
+    return body
+
+
+def handler(event, context):
+
+    print(event)
+
+    params = {
+        "uid": event['requestContext']['authorizer']['claims']['sub']
+    }
+
+    # index = event['queryStringParameters'].get('index', '0')
+    # num = event['queryStringParameters'].get('num', '10')
+
+    return get_thread_notify(**params)
