181a9c64f73ca2359c530856276f817ac60bedc3
Jason Park <joonbin0319@gmail.com>
2023-10-06 22:29:14 +0900

    fix: adding logging to lambda

--- src/lambda/get-thread-notify/index.py
+++ src/lambda/get-thread-notify/index.py
@@ -8,6 +8,11 @@ def get_thread_notify(last_checked_date):
     univ_id = 1
 
     lower_bound_thread_id = f"{last_checked_date}_"
+
+    # Print for debugging
+    print(f"Last checked date received: {last_checked_date}")
+    print(f"Lower bound thread ID: {lower_bound_thread_id}")
+
     # Query the GSI
     response = table.query(
         IndexName='UnivIDbyThreadIDIndex',
@@ -17,6 +22,9 @@ def get_thread_notify(last_checked_date):
     )
 
     count = len(response['Items'])
+
+    # Print for debugging
+    print(f"Number of items retrieved from the query: {count}")
 
     body = JsonPayloadBuilder().add_status(True)\
                                .add_data(count)\
@@ -30,5 +38,8 @@ def handler(event, context):
     params = {
         "last_checked_date": event['queryStringParameters'].get('lastChecked', '20230912201031')
     }
+
+    # Print for debugging
+    print(f"Handler received parameters: {params}")
 
     return get_thread_notify(**params)
