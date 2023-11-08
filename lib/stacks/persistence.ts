32f36ed82eb138929a1ead383cb209fc73b21738
Y.H LIEN <sean.0916413240@gmail.com>
2023-11-08 19:52:25 +0900

    feat: inject-comments2

--- lib/stacks/persistence.ts
+++ lib/stacks/persistence.ts
@@ -67,7 +67,7 @@ export class WasedaTimePersistenceLayer extends PersistenceLayer {
 
     const forumCommentAIDataPipeline = new ForumCommentAIDataPipeline(
       this,
-      'forum-comment-ai-data-pipeline',
+      'forum-comment-ai-data-pipeline', // Error fixed
       {
         // dataSource: syllabusDataPipeline.dataWarehouse,
         threadWareHouse: dynamoDatabase.tables[Collection.THREAD],
