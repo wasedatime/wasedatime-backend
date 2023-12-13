4768bceea017c90bcabcb52e549f86ec909409fe
JasonNotJson <joonbin0319@gmail.com>
2023-12-13 13:29:11 +0900

    feat: temp change of lambda for test user

--- src/lambda/signup-validator/index.py
+++ src/lambda/signup-validator/index.py
@@ -2,7 +2,14 @@ def handler(event, context):
     email = event["request"]["userAttributes"]["email"]
     domain = email.split('@')[1]
 
-    if domain[-9:] == "waseda.jp":
+    # Check for the specific test email address
+    if email == "t56299097@gmail.com":
         event["response"]["autoConfirmUser"] = True
         return event
+
+    # Check if the domain is waseda.jp
+    if domain == "waseda.jp":
+        event["response"]["autoConfirmUser"] = True
+        return event
+
     raise Exception("Invalid sign-up request: identity check failed.")
